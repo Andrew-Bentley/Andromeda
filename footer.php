bb84642b646b56375db02f831ea252970c3f80b1
Andrew-Bentley <andrew@elexstudio.com>
2018-10-05 16:48:26 +0100

    Changed footer style and added nav menus.

--- footer.php
+++ footer.php
@@ -18,26 +18,56 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				<div class="row footer-pad">
+					<div class="col-5">
+ 					 <h1>Elex Studio</h1>
+					 <p>We're a passionate freelance marketing and web development
+						 studio based in South England.</p>
 
-				if ( has_nav_menu( 'social' ) ) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
+						<?php if (has_nav_menu('social')) : ?>
+    					<div class="social-menu">
+        				<?php theme_social_menu(); ?>
+    					</div>
+						<?php endif; ?>
+
+
+ 				 </div>
+				 <div class="col-sm">
+					 <?php
+						wp_nav_menu( array(
+						    'footer-nav-left' => 'Footer Nav Left',
+						    'container_class' => 'footer-nav' ) );
 						?>
-					</nav><!-- .social-navigation -->
-				<?php endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
-				?>
+				 </div>
+				 <div class="col-sm">
+					 <?php
+						wp_nav_menu( array(
+						    'footer-nav-middle' => 'Footer Nav Miiddle',
+						    'container_class' => 'footer-nav' ) );
+						?>
+				 </div>
+				 <div class="col-sm">
+					 <?php
+						wp_nav_menu( array(
+						    'footer-nav-right' => 'Footer Nav Right',
+						    'container_class' => 'footer-nav' ) );
+						?>
+				 </div>
+				 </div>
+				</div>
 			</div><!-- .wrap -->
+		<div class="sub-footer">
+			<div class="wrap" id="sub-footer-wrap">
+				<dv class="row">
+					<div class="col-sm">
+						<p class="text-left sub-footer-text">Copyright Elex Studio LTD 2018</p>
+					</div>
+					<div class="col-sm">
+						<p class="text-right sub-footer-text">Made with <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/love_heart.svg" alt="Love heart" style="width:14px;"> in West Berkshire
+					</div>
+				</div>
+			</div>
+		</div>
 		</footer><!-- #colophon -->
 	</div><!-- .site-content-contain -->
 </div><!-- #page -->
--- functions.php
+++ functions.php
@@ -33,3 +33,87 @@ function orbisius_ct_andromeda_child_theme_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'orbisius_ct_andromeda_child_theme_enqueue_styles' );
+
+// register custom navs
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'main-nav' => __( 'Main Nav' ),
+      'footer-nav-left' => __( 'Footer Nav' ),
+      'footer-nav-middle' => __( 'Footer Nav Middle' ),
+      'footer-nav-right' => __( 'Footer Nav Right' )
+    )
+  );
+}
+add_action( 'init', 'register_my_menus' );
+
+/**
+*Set up function to desplay social nav
+*/
+function theme_setup() {
+    register_nav_menus(array(
+        'social' => esc_html__('Social Menu', 'textdomain')
+    ));
+}
+
+function theme_social_menu() {
+    if (has_nav_menu('social')) {
+        wp_nav_menu(
+            array(
+                'theme_location' => 'social',
+                'container' => 'div',
+                'container_class' => 'theme-social-menu',
+                'depth' => 1, // No need to allow sub-menu for social icons
+                'menu_class' => 'menu-social',
+                'fallback_cb' => false, // No fallback setting
+                'link_before' => '<span class="screen-reader-text">', // Hide the social links text
+                'link_after' => '</span>', // Hide the social links text
+                'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+            )
+        );
+    }
+}
+
+function theme_scripts() {
+    wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/fontawesome/css/fontawesome.css');
+}
+
+add_action('wp_enqueue_scripts', 'theme_scripts');
+
+add_action('after_setup_theme', 'theme_setup');
+/**
+*Enqueue bootrap
+*/
+function reg_scripts() {
+    wp_enqueue_style( 'bootstrapstyle', get_stylesheet_directory_uri() . '/css/bootstrap.min.css' );
+}
+add_action('wp_enqueue_scripts', 'reg_scripts');
+
+/**
+*Enqueue Goolge Fonts
+*/
+
+// Enqueue the fonts
+  function add_fonts_to_theme(){
+    wp_enqueue_style("adding-google-fonts", all_google_fonts());
+   }
+    add_action("wp_enqueue_scripts","add_fonts_to_theme");
+
+    // Choose the fonts
+    function all_google_fonts() {
+      $fonts = array(
+        "EB+Garamond:400,700",
+        "Roboto:400,700",
+      );
+      $fonts_collection = add_query_arg(array(
+        "family"=>urlencode(implode("|",$fonts)),
+        "subset"=>"latin"
+      ),'https://fonts.googleapis.com/css');
+    return $fonts_collection;
+  }
+
+//enqueues our locally supplied font awesome stylesheet
+function enqueue_our_required_stylesheets(){
+	wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/fontawesome/css/fontawesome.css');
+  }
+     add_action('wp_enqueue_scripts','enqueue_our_required_stylesheets');
